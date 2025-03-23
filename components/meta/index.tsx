7eb26332c55d0a616931b31c04f5f992e8f30122
Tran Duy <tranduy10a@gmail.com>
2025-03-23 08:57:00 +0700

    coding homepage

---
+++ components/meta/index.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import Head from 'next/head';
+
+interface MetaProps {
+  title: string;
+}
+
+const Meta: React.FC<MetaProps> = ({ title }) => {
+  return (
+    <Head>
+      <title>{title}</title>
+      <meta
+        name='description'
+        content='Welcome to my profile, I am a front end web developer'
+      />
+      <meta property='og:title' content='My Portfolio' />
+      <meta
+        property='og:description'
+        content='Welcome to my profile, I am a front end web developer'
+      />
+      <meta property='og:url' content='https://tranduy020900dev.vercel/' />
+      <meta property='og:type' content='website' />
+      <link rel='icon' href='/favicon.ico' />
+    </Head>
+  );
+};
+
+export default Meta;
---
+++ components/templates/Homepage/index.tsx
@@ -0,0 +1,115 @@
+import FacebookIcon from '@mui/icons-material/Facebook'
+import GitHubIcon from '@mui/icons-material/GitHub'
+import LinkedInIcon from '@mui/icons-material/LinkedIn'
+import Link from 'next/link'
+import styled from 'styled-components'
+import MainContent from '../../atoms/MainContent'
+import Particle from '../../atoms/Particle'
+import Meta from '../../meta'
+import { NextPageWithLayout } from '../../models/common'
+import { MainLayout } from '../../styles/Layouts'
+
+const Home: NextPageWithLayout = () => {
+  return (
+    <>
+      <Meta title='Home Page' />
+      <MainLayout padding='0'>
+        <HomePageStyled>
+          <div className='particle-con'>
+            <Particle />
+          </div>
+          <div className='typography'>
+            <h1
+              data-aos='zoom-out-left'
+              data-aos-easing='ease-out-cubic'
+              data-aos-duration='2000'
+            >
+              Hi, I&apos;m <span>Duy Tran</span>
+            </h1>
+            <p
+              data-aos='zoom-out-left'
+              data-aos-easing='ease-out-cubic'
+              data-aos-duration='2000'
+            >
+              I&apos;m a Front-end Developer
+            </p>
+            <div className='icons'>
+              <Link href='https://www.facebook.com/tranduy0209/'>
+                <a className='icon i-facebook'>
+                  <FacebookIcon />
+                </a>
+              </Link>
+
+              <Link href='https://github.com/duytran0209'>
+                <a className='icon i-github'>
+                  <GitHubIcon />
+                </a>
+              </Link>
+
+              <Link href='https://www.linkedin.com/in/duytran0209/'>
+                <a className='icon i-linkedin'>
+                  <LinkedInIcon />
+                </a>
+              </Link>
+            </div>
+          </div>
+          <MainContent />
+        </HomePageStyled>
+      </MainLayout>
+    </>
+  )
+}
+
+const HomePageStyled = styled.header`
+  width: 100%;
+  height: 100vh;
+  position: relative;
+  z-index: 1;
+  .typography {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    text-align: center;
+    width: 80%;
+
+    .icons {
+      display: flex;
+      justify-content: center;
+      margin-top: 1rem;
+      .icon {
+        border: 2px solid var(--border-color);
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        border-radius: 50%;
+        transition: all 0.4s ease-in-out;
+        cursor: pointer;
+        &:hover {
+          border: 2px solid var(--primary-color);
+          color: var(--primary-color);
+        }
+        &:not(:last-child) {
+          margin-right: 1rem;
+        }
+        svg {
+          margin: 0.5rem;
+        }
+      }
+
+      .i-youtube {
+        &:hover {
+          border: 2px solid red;
+          color: red;
+        }
+      }
+      .i-github {
+        &:hover {
+          border: 2px solid #5f4687;
+          color: #5f4687;
+        }
+      }
+    }
+  }
+`
+export default Home
